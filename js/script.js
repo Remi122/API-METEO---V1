a4dfb37870283f80025aecb7a1b48f2de8017209
Remi122 <vinelremi12@gmail.com>
2023-01-11 15:19:37 +0100

    first commit

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
-
 var callBackGetSuccess = function(data) {
-    console.log("données api", data)
-    //alert("Meteo temp :" + data.main.temp);
-    var btn= document.querySelector("#icone");
-    btn.addEventListener("click", () => {
-    var element = document.getElementById('#test');
-    element.innerHTML = "Température = " + data.main.temp;
-   })
+  console.log("données api", data)
+  //alert("Meteo temp :" + data.main.temp);
+  var btn= document.querySelector("#icone");
+  btn.addEventListener("click", () => {
+  var element = document.getElementById('#test');
+  element.innerHTML = "Température = " + data.main.temp;
+ })
 }
 
 
@@ -14,19 +13,19 @@ var callBackGetSuccess = function(data) {
 function buttonClickGET() {
 
 
-    var ville = document.getElementById("queryLoc").value;
-    
-    var url = "https://api.openweathermap.org/data/2.5/weather?q="+ ville +"&appid=c21a75b667d6f7abb81f118dcf8d4611&units=metric";
-    
-    $.get(url, callBackGetSuccess).done(function() {
-      //alert( "second sucess" );
-    })
-    .fail(function() {
-      alert( "error" );
-    })
-    .always(function() {
-      //alert( "finished" );
-    }); 
+  var ville = document.getElementById("queryLoc").value;
+  
+  var url = "https://api.openweathermap.org/data/2.5/weather?q="+ ville +"&appid=c21a75b667d6f7abb81f118dcf8d4611&units=metric";
+  
+  $.get(url, callBackGetSuccess).done(function() {
+    //alert( "second sucess" );
+  })
+  .fail(function() {
+    alert( "error" );
+  })
+  .always(function() {
+    //alert( "finished" );
+  }); 
 }
 
 function buttonClickGET()
